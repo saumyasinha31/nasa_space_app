ad859c76795154f8ca470ac6b38f146788e9521e
SAUMYA SINHA <103508561+saumyasinha31@users.noreply.github.com>
2022-10-26 23:54:55 +0530

    Add files via upload

---
+++ tarantula.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+public class tarantula : MonoBehaviour
+{
+    public UnityEvent GVRClick;
+    public float totalTime = 2;
+    bool gvrStatus;
+    public float gvrTimer;
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gvrStatus)
+        {
+            gvrTimer += Time.deltaTime;
+            
+        }
+        if(gvrTimer > totalTime)
+        {
+            GVRClick.Invoke();
+        }
+        
+    }
+    public void GvrOn()
+    {
+        gvrStatus = true;
+    }
+    public void GvrOff()
+    {
+        gvrStatus = false;
+        gvrTimer = 0;
+    }
+}
